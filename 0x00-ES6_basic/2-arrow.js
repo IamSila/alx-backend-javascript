4212e5ffc7b9a056db9b74740d98f0c51f2e82ff
IamSila <silamulingi@gmail.com>
2024-10-04 11:40:11 +0300

    corrected the comments spacing

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
 export default function getNeighborhoodList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  /** converting this fucntion to an arrow function*/
+  /** converting this fucntion to an arrow function */
   this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
