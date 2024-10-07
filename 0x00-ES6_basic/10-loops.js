b5a207af967cdc0b1d9dad6dc784cf7a47a5b79c
IamSila <silamulingi@gmail.com>
2024-10-07 07:10:02 +0300

    changed return statement

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
     value = `${appendString} ${value}`;
+    return [value];
   }
-
-  return array;
 }
