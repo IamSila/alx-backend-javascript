279968ed8756678c93f455b2136135153b54f639
IamSila <silamulingi@gmail.com>
2024-10-20 11:32:13 +0300

    more changes on export

--- 0x02-ES6_classes/9-main.js
+++ 0x02-ES6_classes/9-main.js
@@ -2,8 +2,8 @@ import listOfStudents from "./9-hoisting.js";
 
 console.log(listOfStudents);
 
-const listPrinted = listOfStudents.map(
-  (student) => student.fullStudentDescription,
-);
+// const listPrinted = listOfStudents.map(
+//   (student) => student.fullStudentDescription,
+// );
 
-console.log(listPrinted);
+// console.log(listPrinted);
