bf239d8e22ceba87223992f4c82fe38e91f51385
IamSila <silamulingi@gmail.com>
2024-10-26 10:54:01 +0300

    identation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -6,7 +6,7 @@ export default function getStudentsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
       (prevStudent, curStudent) => prevStudent.id || prevStudent.id + curStudent.id,
-            0,
+        0,
     );
   }
   return 0;
