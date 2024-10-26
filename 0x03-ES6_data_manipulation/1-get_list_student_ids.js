d014b8e1cd851cb077b1b9c7b5dac4e2441c0e08
IamSila <silamulingi@gmail.com>
2024-10-26 07:32:25 +0300

    corrected spacing and identation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -4,9 +4,9 @@
  */
 
 export default function getListStudentIds(students) {
-    if (students instanceof Array) {
-        return students.map((student) => student.id);
-    }
-    /** if not an instance of an array */
-    return [];
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  /** if not an instance of an array */
+  return [];
 }
