8a74b8a6bdeaddc21a6faead4c29af1f1d914b26
IamSila <silamulingi@gmail.com>
2024-10-26 07:35:30 +0300

    more corrections

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,6 @@
-/** 
- * function getListStudentIds
- * 
- */
-
 export default function getListStudentIds(students) {
   if (students instanceof Array) {
     return students.map((student) => student.id);
   }
-  /** if not an instance of an array */
-  return [];
+	return [];
 }
