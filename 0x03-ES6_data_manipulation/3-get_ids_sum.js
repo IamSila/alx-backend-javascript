54ebd05db204a776a9ad0d30d1441b34be4cc2ce
IamSila <silamulingi@gmail.com>
2024-10-26 10:55:34 +0300

    corrections

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,12 +1,8 @@
-/** create a fucntion getStudentsSum
- * 
- */
-
-export default function getStudentsSum(students) {
+export default function getStudentIdsSum(students) {
   if (students instanceof Array) {
     return students.reduce(
-      (prevStudent, curStudent) => prevStudent.id || prevStudent.id + curStudent.id,
-        0,
+    (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+    0,
     );
   }
   return 0;
