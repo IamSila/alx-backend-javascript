187a804bf76e4e44eb7de87a887c10f3943eaf53
IamSila <silamulingi@gmail.com>
2024-10-26 10:47:17 +0300

    reduce of maps in js

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,13 @@
+/** create a fucntion getStudentsSum
+ * 
+ */
+
+export default function getStudentsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+        (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+            0,
+        );
+    }
+    return 0;
+}
